2c26ab8defce4addfadacaef6eacee8874deb498
Subhajit Sahu <wolfram77@gmail.com>
2015-05-27 23:28:57 +0530

    Image Load and Save

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,21 @@
 // @wolfram77
 package main;
 
+// required modules
+import java.awt.image.*;
+import javax.imageio.*;
+import java.util.*;
+import java.io.*;
+
 
 
 public class Main {
     
+    public static void main(String[] args) throws Exception {
+        BufferedImage img = ImageIO.read(new File("0.jpg"));
+        ImageIO.write(img, "jpg", new File("1.jpg"));
+        for(Iterator<ImageWriter> i = ImageIO.getImageWritersByFormatName("bmp"); i.hasNext();)
+            System.out.println(i.next());
+        System.out.println(img);
+    }
 }
