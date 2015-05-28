54aa988836326ee99cd98b6a1f4768222d141d9b
Subhajit Sahu <wolfram77@gmail.com>
2015-05-28 22:35:51 +0530

    Image Compression Quality

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,6 @@ package main;
 // required modules
 import java.awt.image.*;
 import javax.imageio.*;
-import java.util.*;
 import java.io.*;
 
 
@@ -13,9 +12,13 @@ public class Main {
     
     public static void main(String[] args) throws Exception {
         BufferedImage img = ImageIO.read(new File("0.jpg"));
-        ImageIO.write(img, "jpg", new File("1.jpg"));
-        for(Iterator<ImageWriter> i = ImageIO.getImageWritersByFormatName("bmp"); i.hasNext();)
-            System.out.println(i.next());
+        // ImageIO.write(img, "jpg", new File("1.jpg"));
+        ImageWriter wrt = ImageIO.getImageWritersByFormatName("jpeg").next();
+        ImageWriteParam iwp = wrt.getDefaultWriteParam();
+        iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        iwp.setCompressionQuality(0.5f);
+        wrt.setOutput(ImageIO.createImageOutputStream(new File("1.jpg")));
+        wrt.write(null, new IIOImage(img, null, null), iwp);
         System.out.println(img);
     }
 }
